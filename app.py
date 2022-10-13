d09221c2478281e0730fc85df4865c51f34dcbcc
zyzyzyclaire <kunji117@naver.com>
2022-10-13 19:56:58 +0900

    temp

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", text="Hello World!")
+    return render_template("index.html", shinramg="김아무개", shinboo="이아무개")
 
 if __name__ == '__main__':
     app.run()
