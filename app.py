15559db542bb7ee75d7356e5cbe6689a7b393745
zyzyzyclaire <kunji117@naver.com>
2022-10-17 22:42:55 +0900

    temp

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", shinramg="김아무개", shinboo="이아무개")
+    return render_template("index.html", groom="김아무개", bride="이아무개", 
+    main_pic='images/main_pic.png', wedding_hall='A호텔 웨딩홀',
+    pic2='images/pic2.png')
 
 if __name__ == '__main__':
     app.run()
