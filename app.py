72b4f608b0b2ac8ed1ce57e9f92cd30cbe1e465b
zyzyzyclaire <kunji117@naver.com>
2022-10-25 20:04:00 +0900

    mysql연경

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 
+from model import session_scope, test, test_from_vscode
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    with session_scope() as session:
+        test_name_1 = session.query(test.test_name).all()
+        print([i for i in test_name_1])
+
+        # test_from_vscode_item = test_from_vscode(1,'test_name')
+        # session.add(test_from_vscode_item)
+        # session.commit()
+        # session.refresh(test_from_vscode_item)
+
     return render_template("index.html", 
-                            groom="김아무개", 
+                            groom=test_name_1, 
                             bride="이아무개", 
                             main_pic='images/main_pic.png', 
                             wedding_hall='A호텔 웨딩홀',
