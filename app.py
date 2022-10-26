beedcc8bb2baaff28f14670918db2b90adf8c1cf
whrksp126 <whrksp126@naver.com>
2022-10-27 00:24:33 +0900

    pull main

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
-import os
 from config import ApplicationConfig
 
+# 확장을 생성
 db = SQLAlchemy()
-
+# create the app
 app = Flask(__name__)
+# 앱 인스턴스 폴더를 기준으로 SQLite 데이터베이스 구성
 
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://' + user + ":" + passwd + '@' + host + "/" + database
 app.config.from_object(ApplicationConfig)
 
+# 확장 프로그램으로 앱 초기화
 db.init_app(app)
 
 from models import User
+
+@app.route("/")
+def index():
+    return render_template("index.html", groom='test_name_1', bride="이아무개", main_pic='images/main_pic.png', wedding_hall='A호텔 웨딩홀',
+                            pic2='images/pic2.png', account1=12345, account2=67890, account3=98765, account4=43210)
     
 with app.app_context():
     db.create_all()
--- models.py
+++ models.py
@@ -4,3 +4,8 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(250))
+
+class User2(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  email = db.Column(db.String(250))
